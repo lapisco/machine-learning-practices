9518984c92e6d9d4d2ee46ea3851640312603788
Leandro Bezerra Marinho <leandro.marinho@lapisco.ifce.edu.br>
2019-03-28 22:05:09 -0300

    features description

--- 1_distance.py
+++ 1_distance.py
@@ -1,10 +1,11 @@
 from scipy.spatial import distance
 
-# X1. esférico.
-# X2. sim.
-# X3. alaranjado.
-# X4. rugosa.
-# X5. sim.
+# X1. Formato (esférico/oval)?
+# X2. Fruta cítrica?
+# X3. Cor?
+# X4. Casca lisa ou rugosa?
+# X5. Cheiro ativo?
+
 
 laranja     = [0, 1, 2, 1, 0]
 maca        = [0, 0, 1, 0, 0]
@@ -13,7 +14,7 @@ tangerina   = [0, 1, 2, 1, 1]
 
 
 print(distance.cityblock(laranja, tangerina))
-# print(distance.euclidean(laranja, tangerina))
+print(distance.euclidean(laranja, tangerina))
 
-# print(distance.euclidean(maca, tangerina))
+print(distance.euclidean(maca, tangerina))
 print(distance.cityblock(maca, tangerina))
